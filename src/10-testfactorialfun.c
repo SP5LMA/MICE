bef8b525f95fd1f37d2e2e10efe25efc34835666
Student IETiSIP <stud@ubuntu_1804_desktop.iem.pw.edu.pl>
2020-01-15 12:49:11 +0100

    Fixed formatting string.
    	modified:   10-testfactorialfun.c

--- src/10-testfactorialfun.c
+++ src/10-testfactorialfun.c
@@ -4,6 +4,6 @@ extern unsigned long factorial(unsigned long);
 int main(void){
     unsigned long k;
     for (k=0; k<= 22; k++)
-        printf("%u! = %lu\n", k, (unsigned long) factorial(k));
+        printf("%lu! = %lu\n", k, (unsigned long) factorial(k));
     return 0;
 }
