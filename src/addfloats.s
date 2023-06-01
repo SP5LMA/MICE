aed7af009c185a1b34f1da0c6702b58b6ec9c8e7
Lukasz <lukasz.makowski.ee@pw.edu.pl>
2023-06-02 01:34:31 +0200

    Add two more samples covering FPU
    - improvement to content
    - sample code adds two floats
    - sample code multiplies two doubles

---
+++ src/addfloats.s
@@ -0,0 +1,25 @@
+.include "include/consts.s"
+
+.data
+	.align 2
+	val1F:	.float 2.1
+	val2F:	.float 3.7
+
+.bss
+	valResultF:	.double
+
+.text
+.global _start
+_start:
+	ldr r0, =val1F
+	vldr.f32 s0, [r0]
+	ldr r1, =val2F
+	vldr.f32 s1, [r1]
+
+	vadd.f32 s2, s1, s0
+
+	ldr r2, =valResultF
+	vstr.f32 s2, [r2]
+
+	mov r0, #EXIT_SUCCESS
+	bl exit
---
+++ src/muldoubles.s
@@ -0,0 +1,35 @@
+.include "include/consts.s"
+
+.data
+	.align 2
+	val1F:	.double 2.1
+	val2F:	.double 3.7
+	fmt:	.asciz "%.3lf * %.3f = %.3lf\n"
+
+.text
+.global _start
+_start:
+
+	push {fp, lr}
+	add fp, sp, #4
+	sub sp, sp, #40
+
+	ldr r0, =val1F
+	vldr.f64 d0, [r0]
+	ldr r1, =val2F
+	vldr.f64 d1, [r1]
+
+	vmul.f64 d2, d1, d0
+
+	vmov r2, r3, d0
+	vstr.f64 d1, [sp]
+	vstr.f64 d2, [sp, #8]
+
+	ldr r0, =fmt
+	bl printf
+
+	sub sp, fp, #4
+	pop {fp, lr}
+
+	mov r0, #EXIT_SUCCESS
+	bl exit
