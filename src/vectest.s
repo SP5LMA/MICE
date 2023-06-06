de914565b1d4ab88a21f0b62ec078f206c83e450
Lukasz Makowski <lukasz.makowski.ee@pw.edu.pl>
2023-06-06 13:17:30 +0200

    Adds new example for SIMD
     - vectest loads two vectors and multiplies them

---
+++ src/vectest.s
@@ -0,0 +1,28 @@
+.include "include/syscalls-eabi.s"
+.include "include/consts.s"
+
+.data
+	.align	2
+	vec1F:	.float 1.0, 2.0, 3.0, 4.0
+	vec2F:	.float 5.0, 6.0, 7.0, 8.0
+
+.bss
+	vecOut:	.skip 16
+
+.text
+.global _start
+_start:
+	ldr r0, =vec1F
+	ldr r1, =vec2F
+	
+	vld1.32 {d0-d3}, [r0]
+	vld1.32 {d4-d7}, [r1]
+	
+	vmul.f32 q0, q0, q2
+	
+	ldr r2, =vecOut
+	vst1.32 {d0-d3}, [r2]
+    
+	mov r0, #EXIT_SUCCESS
+	mov r7, #sys_exit
+	svc #0
