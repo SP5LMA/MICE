b355c09f887857168b2a5784bd472c52a5cd4c96
Lukasz Makowski <lukasz.makowski.ee@pw.edu.pl>
2025-03-24 13:21:51 +0100

    Fix: IT block must be used
    - addcs should be preceded with IT block

--- src/checksum.s
+++ src/checksum.s
@@ -4,9 +4,9 @@
 
 .data
 .align	2
-	str1: .asciz "0123"
-  str2: .asciz "I have no special talent. I am only passionately curious.\n"
-	
+    str1: .asciz "0123"
+    str2: .asciz "I have no special talent. I am only passionately curious.\n"
+    
 .text
 .align	2
 .global	main
@@ -15,20 +15,21 @@
 .type	main, %function
 .thumb_func
 main:
-	mov r0, #0
-	ldr r1, =str1
-	ldrb r2, [r1], #1
-	cmp r2, #0
-	beq finish
+    mov r0, #0
+    ldr r1, =str1
+    ldrb r2, [r1], #1
+    cmp r2, #0
+    beq finish
 shift:
-	lsrs r2, #1
-	addcs r0, #1
-	bne shift
-  nop
-  nop
-  nop
+    lsrs r2, #1
+    IT cs
+    addcs r0, #1
+    bne shift
+    nop
+    nop
+    nop
 
 finish:
-	mov	r0, #EXIT_SUCCESS
-	mov	r7, #sys_exit
-	svc	#0
+    mov	r0, #EXIT_SUCCESS
+    mov	r7, #sys_exit
+    svc	#0
